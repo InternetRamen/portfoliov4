feefa4772e5cdb5f05152c98d73d97c25e73cc65
unknown <jadenhou19@gmail.com>
2021-11-28 19:53:39 -0500

    initial commit

---
+++ public/scripts/onload.js
@@ -0,0 +1,21 @@
+let itemList = document.querySelector(".main-list")
+let title = document.querySelector(".title")
+let h2 = document.querySelector(".subtitle")
+function delay(ms) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve() }, ms);
+    })
+}
+async function a() {
+    await delay(200);
+    title.classList.remove("slide-right")
+    await delay(200);
+    h2.classList.remove("slide-right");
+    await delay(200);
+    for (let item of itemList.children) {
+    await delay(100)
+    item.classList.remove("slide-in")
+} 
+
+}
+a()
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const http = require("http")
+
+//set up
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const indexRouter = require("./routes/index")
+app.use("/", indexRouter)
+
+let port = 3000
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(
+        `Website running at http://localhost:${process.env.PORT || "3000"}`
+    );
+
+})
